5c0e564ba56018a7fef367acfb469b6b259e6f23
Paweł Jurek <pj302000@student.polsl.pl>
2023-10-04 10:34:20 +0200

    visually working modifying dish quantity in order

--- restaurant/orders/templatetags/order_tags.py
+++ restaurant/orders/templatetags/order_tags.py
@@ -4,4 +4,7 @@ register = Library()
 
 @register.inclusion_tag('tags/show_price.html')
 def show_price(item):
-    return {'total_price': round(item.dish.price * item.quantity, 3)}
+    return {
+        'total_price': f'{round(item.dish.price * item.quantity, 2):.2f}',
+        'id': item.pk
+            }
--- restaurant/static/modify_dish_quantity.js
+++ restaurant/static/modify_dish_quantity.js
@@ -2,29 +2,26 @@ document.addEventListener('DOMContentLoaded', function() {
     const add_buttons = document.querySelectorAll('.increase-amount');
     const delete_buttons = document.querySelectorAll('.decrease-amount');
     //var csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+    change_quantity(add_buttons, 1);
+    change_quantity(delete_buttons, -1);
     
-    add_buttons.forEach(button => {
-        button.addEventListener('click', function(){
-            dish_id = button.getAttribute('data-quantity');
-            var quantity_p = document.querySelector(`#quantity${dish_id}`);
-            console.log('num', quantity_p.textContent);
-            num = parseInt(quantity_p.textContent);
-            
-            num += 1;
-            quantity_p.textContent = num;
-        })
-    });
-
-    delete_buttons.forEach(button => {
-        button.addEventListener('click', function(){
-            dish_id = button.getAttribute('data-quantity');
-            var quantity_p = document.querySelector(`#quantity${dish_id}`);
-            num = parseInt(quantity_p.textContent);
-            if (num > 0) {
-                num -= 1;
-            }     
-            quantity_p.textContent = num;
-        })
-    });
 
 })
+
+function change_quantity(buttons, number){
+    buttons.forEach(button => {
+        button.addEventListener('click', function(){
+            const dish_id = button.getAttribute('data-quantity');
+            const dish_price = button.getAttribute('data-price');
+            var quantity_p = document.querySelector(`#quantity${dish_id}`);
+            price_p = document.querySelector(`#price${dish_id}`);
+            var quantity = parseInt(quantity_p.textContent);
+            
+            if (quantity > 0 || number > 0) {
+                quantity += number;
+                quantity_p.textContent = quantity;
+                price_p.textContent = `${(dish_price*quantity).toFixed(2)} zł`;
+            }         
+        })
+    });
+}
